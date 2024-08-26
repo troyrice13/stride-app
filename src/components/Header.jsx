912dde54693220c56a814a36d379d5af35090038
Troy Rice <troyanthonyrice@gmail.com>
2024-08-26 13:41:31 -0700

    auth0, LoginButton

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
     return(
-        <nav className="flex justify-center mt-4 bg-blue-400 text-white">
-            <Link to="/">Home</Link>
-            <Link to="/itinerary" className="ml-4">Itinerary</Link>
-            <Link to="/login" className="ml-4">Login</Link>
-            <Link to="/register" className="ml-4">Register</Link>
+        <nav className="flex justify-center mt-6 bg-blue-400 text-white bg-white border-gray-200 dark:bg-gray-900">
+            <Link className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" to="/">Home</Link>
+            <Link to="/itinerary" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent ml-4">Itinerary</Link>
+            <Link to="/login" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent ml-4 ml-4">Login</Link>
+            <Link to="/register" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent ml-4 ml-4">Register</Link>
         </nav>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,10 +13,10 @@ export default function Home() {
 
     return (
         <div className="flex justify-center items-center h-screen w-screen flex-col">
-            <h1 className='text-4xl font-medium'>Home</h1>
+            <h1 className='text-4xl font-medium mb-4'>Home</h1>
             <span>
-                <button className="rounded-lg border border-transparent px-4 py-2 text-base font-medium bg-[#f9f9f9] cursor-pointer transition-colors duration-200 focus:outline focus:outline-[4px] focus:outline-auto focus:outline-webkit-focus-ring-color" onClick={navToLogin}>Login</button>
-                <button onClick={navToRegister}className="rounded-lg border border-transparent px-4 py-2 text-base font-medium bg-[#f9f9f9] cursor-pointer transition-colors duration-200 focus:outline focus:outline-[4px] focus:outline-auto focus:outline-webkit-focus-ring-color">Register</button>
+                <button className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-xl text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" onClick={navToLogin}>Login</button>
+                <button onClick={navToRegister}className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-xl text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" >Register</button>
             </span>
         </div>
     )
